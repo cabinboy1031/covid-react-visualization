51d97485a92a5ed8cf24c6dabc37bdeef3caa4c5
mysticCabinboy <ian@mysticcabin.xyz>
2021-03-06 16:33:53 -0800

    Implemented selecting different parts of the data set.

--- src/components/CovidVisuals.js
+++ src/components/CovidVisuals.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import ReactDOMServer from 'react-dom/server';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import ToggleButtonGroup from 'react-bootstrap/ToggleButtonGroup'
+import ToggleButton from 'react-bootstrap/ToggleButton'
+
+import randomColor from 'randomcolor';
 
 import { AreaChart,
          Area,
@@ -14,7 +19,8 @@ class CovidAreaGraph extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            response: {}
+            response: {},
+            activeAreas: [],
         }
     }
 
@@ -25,18 +31,45 @@ class CovidAreaGraph extends React.Component {
                 this.setState({
                     response: data.reverse(),
                 });
-                console.log(this.state.response);
             });
     }
 
     render(){
+        const setAreas = (val) => {
+            var areas = [];
+            for(var i = 0; i < val.length; i++){
+                areas.push(val[i]);
+            }
+            console.log(areas);
+            this.setState({activeAreas: areas});
+        };
+        const getAreas = () => {
+            var renderedAreas = [];
+            for(var i = 0; i < this.state.activeAreas.length; i++){
+                renderedAreas.push(
+                    <Area type="monotone" dataKey={this.state.activeAreas[i]} stroke={randomColor()}/>
+                );
+            }
+            return renderedAreas;
+        }
+
         return(
                 <Row>
-                    <Col lg={4}> This is the details column </Col>
+                    <Col lg={4}>
+                        <h2> total hospitalizations compared to number of deaths</h2>
+                        <p>
+                            This is extremely easy to add and remove data points. Choose some of the options below
+                            to see some of the results.
+                        </p>
+
+                        <ToggleButtonGroup size="lg" type="checkbox" variant="secondary" onChange={setAreas} vertical={true}>
+                            <ToggleButton value="hospitalizedCurrently" checked={true}>Hospitalized</ToggleButton>
+                            <ToggleButton value="deathIncrease" checked={true}>Deaths by Day</ToggleButton>
+                        </ToggleButtonGroup>
+                    </Col>
                     <Col lg={8}>
                         <AreaChart width={1000} height={700} data={this.state.response} margin={{top:5, right:20, bottom:50, left:20}}>
-                            <Area type="monotone" dataKey="hospitalizedCurrently" stroke="#4484ff" />
-                            <Area type="monotone" dataKey="deathIncrease" stroke="#000000" />
+                            {getAreas()}
                             <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
                             <XAxis dataKey="date" angle={-45} interval={10} textAnchor="end"/>
                             <YAxis />
